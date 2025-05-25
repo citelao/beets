e3feae9fb5e3b543706f41ab038deb6e8e9b045a
Ben Stolovitz <ben@stolovitz.com>
2025-05-25 15:39:55 -0400

    avoid linter error

--- test/plugins/test_lyrics.py
+++ test/plugins/test_lyrics.py
@@ -33,7 +33,8 @@ github_ci = os.environ.get("GITHUB_ACTIONS") == "true"
 if not github_ci and not importlib.util.find_spec("langdetect"):
     pytest.skip("langdetect isn't available", allow_module_level=True)
 
-from beetsplug import lyrics
+# Ignore E402: ensure we have langdetect before trying to import the lyrics plugin.
+from beetsplug import lyrics # noqa: E402
 
 PHRASE_BY_TITLE = {
     "Lady Madonna": "friday night arrives without a suitcase",
